b14b9138b42b2c87a9446178d71983b2b1dab10e
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2019-09-24 16:37:32 -0400

    Swift 5.1 moved all XML objects to FoundationXML, so added check for swift >= 4.1 can use the canImport check to see if FoundationXML exists for import

--- Sources/RawRepresentableHelpers/XMLNode.swift
+++ Sources/RawRepresentableHelpers/XMLNode.swift
@@ -6,6 +6,11 @@
 //
 
 import Foundation
+#if swift(>=4.1)
+    #if canImport(FoundationXML)
+        import FoundationXML
+    #endif
+#endif
 
 public extension XMLNode {
     /// Returns an XMLElement object with a given tag identifier, or name
